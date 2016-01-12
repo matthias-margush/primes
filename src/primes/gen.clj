b1e996506787d756dfa9bff00b8937206ce198f9
Matthias Margush <matthias.margush@gmail.com>
2016-01-12 09:46:17 -0800

    add dividers to the table

--- src/primes/gen.clj
+++ src/primes/gen.clj
@@ -27,37 +27,52 @@
 
 
 (defn fmt
-  "Returns the seq with each item formatted for printing."
-  [width v]
+  "Prepends val with enough spaces so it fits in width, and appends vals"
+  [width val & vals]
   (let [fmt-str (str "%" width "s")]
-    (format fmt-str v)))
+    (apply str (format fmt-str val) vals)))
+
+
+(defn spacer
+  "Creates a spacer by repeating ch of the given width"
+  [width s]
+  (join "" (take width (repeat s))))
 
 
 (defn print-table
   "Prints a multiplication table of primes."
   []
   (let [f *
+        v-div " |"
+        h-div "-"
+
         primes (take 10 (primes))
 
+        ; label data
         row-headers primes
         col-headers primes
 
+        ; table data
         rows (products f row-headers col-headers)
         cols (transpose rows)
 
+        ; layout widths
         col-widths (map max-width cols)
         row-lbl-width (first col-widths)
-        
-        col-labels (map fmt col-widths col-headers)
-        row-labels (map fmt (repeat row-lbl-width) row-headers)
-        row-values (for [row rows] (map fmt col-widths row))]
+
+        ; labels
+        topleft-lbl (spacer (+ (count v-div) row-lbl-width) " ")
+        row-lbls (map #(fmt row-lbl-width % v-div) row-headers)
+        col-lbls (cons topleft-lbl (map fmt col-widths col-headers))
+        row-vals (for [row rows] (map fmt col-widths row))
+
+        ; render
+        header-str (join " " col-lbls)
+        divider-str (spacer (count header-str) "-")
+        rows-strs (map #(str %1 " " (join " " %2)) row-lbls row-vals)]
     
-    ; column header row
-    (println (fmt row-lbl-width " ")(join " " col-labels))
-
-    ; each row with labels
-    (dorun
-      (map #(println %1 (join " " %2)) row-labels row-values))))
-
-
+    (println header-str)
+    (println divider-str)
+    (doseq [row-str rows-strs]
+           (println row-str))))
 
