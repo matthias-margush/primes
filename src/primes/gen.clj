222c102b5ff8dbe9c7817102f4bc5736b51c00e0
Matthias Margush <matthias.margush@gmail.com>
2016-01-12 10:50:37 -0800

    implemented prime

--- src/primes/gen.clj
+++ src/primes/gen.clj
@@ -1,11 +1,31 @@
 (ns primes.gen
-    (:require [clojure.string :refer [join]]))
+    (:require [clojure.string :refer [join]]
+              [clojure.math.numeric-tower :refer [sqrt]]))
+
+
+(defn- prime?
+  "Checks whether a number is prime."
+  [p?]
+  (let [end (sqrt p?)]
+    (loop [n 2]
+      (cond
+        (> n end) true
+        (= 0 (rem p? n)) false
+        :else (recur (inc n))))))
+
+
+(defn- next-prime
+  "Returns the next highest prime, given an initial prime number"
+  [p]
+  (loop [n (inc p)]
+    (if (prime? n) n
+      (recur (inc n)))))
 
 
 (defn primes
   "Returns a lazy seq of primes."
-  []
-  (rest (range)))
+  ([] (primes 1))
+  ([p] (lazy-seq (cons p (primes (next-prime p))))))
 
 
 (defn products
